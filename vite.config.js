37437571206b479e0bd2885441ad13142b694915
Yongyeon Kim <seyeon.dev@gmail.com>
2025-04-15 14:37:43 +0900

    fix: 자동 import 안 돼서 이전 방식으로 되돌림

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
-    alias: {
-      "@": path.resolve("./src"),
-    },
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
 });
