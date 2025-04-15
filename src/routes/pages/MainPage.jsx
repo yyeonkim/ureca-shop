15907fa1a4199d3abf369c6e44d67e0e3475d98a
Yongyeon Kim <seyeon.dev@gmail.com>
2025-04-15 11:36:31 +0900

    refactor: 이미지 최적화

--- src/routes/pages/MainPage.jsx
+++ src/routes/pages/MainPage.jsx
@@ -6,12 +6,16 @@ import { Swiper, SwiperSlide } from "swiper/react";
 const slideImgs = [
   {
     src: "/slides/slide1.jpg",
-    srcSet: "/slides/slide1-mobile.jpg",
+    srcSetMoJPG: "/slides/slide1-mobile.jpg",
+    srcSetMoWebP: "/slides/slide1-mobile.webp",
+    srcSetPCWebP: "/slides/slide1.webp",
     alt: "Gold big hoops",
   },
   {
     src: "/slides/slide2.jpg",
-    srcSet: "/slides/slide2-mobile.jpg",
+    srcSetMoJPG: "/slides/slide2-mobile.jpg",
+    srcSetWebP: "/slides/slide2-mobile.webp",
+    srcSetPCWebP: "/slides/slide2.webp",
     alt: "Fast fashion, and faster fashion",
   },
 ];
@@ -36,9 +40,14 @@ export default function MainPage() {
         {slideImgs.map((slide) => (
           <SwiperSlide>
             <picture className={styles.swiperPicture}>
-              {/* Mobile */}
-              <source srcSet={slide.srcSet} media="(max-width: 768px)" />
-              <img src={slide.src} alt={slide.alt} />
+              {/* Mobile WebP */}
+              <source srcSet={slide.srcSetMoWebP} type="image/webp" media="(max-width: 768px)" />
+              {/* Mobile JPG */}
+              <source srcSet={slide.srcSetMoJPG} type="image/jpeg" media="(max-width: 768px)" />
+              {/* Desktop WebP */}
+              <source srcSet={slide.srcSetPCWebP} type="image/webp" media="(min-width: 768px)" />
+              {/* Desktop JPG */}
+              <img src={slide.src} alt={slide.alt} loading="lazy" />
             </picture>
             <div className={styles.slideContent}>
               <h2>{slide.alt}</h2>
