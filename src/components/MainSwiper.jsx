d40f6e806c1827642ca96ac8301a89c07fca60b2
Yongyeon Kim <seyeon.dev@gmail.com>
2025-04-15 15:37:32 +0900

    refactor: swiper 코드 분할

---
+++ src/components/MainSwiper.jsx
@@ -0,0 +1,62 @@
+import styles from "@/styles/MainSwiper.module.css";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import MainButton from "./MainButton.jsx";
+
+const slideImgs = [
+  {
+    src: "/slides/slide1.jpg",
+    srcSetMoJPG: "/slides/slide1-mobile.jpg",
+    srcSetMoWebP: "/slides/slide1-mobile.webp",
+    srcSetPCWebP: "/slides/slide1.webp",
+    alt: "Gold big hoops",
+  },
+  {
+    src: "/slides/slide2.jpg",
+    srcSetMoJPG: "/slides/slide2-mobile.jpg",
+    srcSetWebP: "/slides/slide2-mobile.webp",
+    srcSetPCWebP: "/slides/slide2.webp",
+    alt: "Fast fashion, and faster fashion",
+  },
+];
+
+function MainSwiper() {
+  return (
+    <Swiper
+      spaceBetween={30}
+      centeredSlides={true}
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Autoplay, Pagination, Navigation]}
+      className={styles.swiper}
+    >
+      {slideImgs.map((slide) => (
+        <SwiperSlide>
+          <picture className={styles.swiperPicture}>
+            {/* Mobile WebP */}
+            <source srcSet={slide.srcSetMoWebP} type="image/webp" media="(max-width: 768px)" />
+            {/* Mobile JPG */}
+            <source srcSet={slide.srcSetMoJPG} type="image/jpeg" media="(max-width: 768px)" />
+            {/* Desktop WebP */}
+            <source srcSet={slide.srcSetPCWebP} type="image/webp" media="(min-width: 768px)" />
+            {/* Desktop JPG */}
+            <img src={slide.src} alt={slide.alt} />
+          </picture>
+          <div className={styles.slideContent}>
+            <h2>{slide.alt}</h2>
+            <h3>$ 68,00</h3>
+            <MainButton>View Product</MainButton>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
+
+export default MainSwiper;
--- src/routes/pages/MainPage.jsx
+++ src/routes/pages/MainPage.jsx
@@ -1,63 +1,16 @@
-import MainButton from "@/components/MainButton.jsx";
+// import MainSwiper from "@/components/MainSwiper.jsx";
 import styles from "@/styles/Main.module.css";
-import { Autoplay, Navigation, Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
 
-const slideImgs = [
-  {
-    src: "/slides/slide1.jpg",
-    srcSetMoJPG: "/slides/slide1-mobile.jpg",
-    srcSetMoWebP: "/slides/slide1-mobile.webp",
-    srcSetPCWebP: "/slides/slide1.webp",
-    alt: "Gold big hoops",
-  },
-  {
-    src: "/slides/slide2.jpg",
-    srcSetMoJPG: "/slides/slide2-mobile.jpg",
-    srcSetWebP: "/slides/slide2-mobile.webp",
-    srcSetPCWebP: "/slides/slide2.webp",
-    alt: "Fast fashion, and faster fashion",
-  },
-];
+import { lazy, Suspense } from "react";
+const MainSwiper = lazy(() => import("@/components/MainSwiper.jsx"));
 
 export default function MainPage() {
   return (
     <>
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className={styles.swiper}
-      >
-        {slideImgs.map((slide) => (
-          <SwiperSlide>
-            <picture className={styles.swiperPicture}>
-              {/* Mobile WebP */}
-              <source srcSet={slide.srcSetMoWebP} type="image/webp" media="(max-width: 768px)" />
-              {/* Mobile JPG */}
-              <source srcSet={slide.srcSetMoJPG} type="image/jpeg" media="(max-width: 768px)" />
-              {/* Desktop WebP */}
-              <source srcSet={slide.srcSetPCWebP} type="image/webp" media="(min-width: 768px)" />
-              {/* Desktop JPG */}
-              <img src={slide.src} alt={slide.alt} loading="lazy" />
-            </picture>
-            <div className={styles.slideContent}>
-              <h2>{slide.alt}</h2>
-              <h3>$ 68,00</h3>
-              <MainButton>View Product</MainButton>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-
+      <Suspense fallback={<p className={styles.loading}>Loading...</p>}>
+        <MainSwiper />
+      </Suspense>
+      {/* <MainSwiper /> */}
       <section className={styles.shopSection}>
         <div>
           <h2>Shop The Latest</h2>
