ac0e9acb383e4a85fa2e6023c1c77704a77924ae
Yongyeon Kim <seyeon.dev@gmail.com>
2025-04-17 10:28:51 +0900

    feat: Footer 스타일링

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,26 @@
 import styles from "@/styles/Footer.module.css";
+import { Link } from "react-router";
 
 export default function Footer() {
-  return <footer className={`${styles.footer} mw`}>Footer</footer>;
+  return (
+    <footer className={`${styles.footer} mw`}>
+      <div>
+        <nav>
+          <Link to="#">CONTACT</Link>
+          <Link to="#">TERMS OF SERVICES</Link>
+          <Link to="#">SHIPPING AND RETURNS</Link>
+        </nav>
+        <div>Give an email, get the newsletter.</div>
+      </div>
+      <div>
+        <p>© 2021 Shelly. Terms of use and privacy policy.</p>
+        <div className={styles.sns}>
+          <i>L</i>
+          <i>F</i>
+          <i>I</i>
+          <i>T</i>
+        </div>
+      </div>
+    </footer>
+  );
 }
